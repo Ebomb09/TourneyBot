5aeef221f1063aae94cedd866cec58e198f42b52
Hayden <hayden@thedunstans.com>
2021-02-06 19:31:59 -0800

    save

--- profile.py
+++ profile.py
@@ -1,13 +1,16 @@
+from leaderboard import Leaderboard
+
 class Profile:
-    """ Profile represents a person with their name, wins, losses and position on the leaderboard
+    """ Profile represents a person with their name, wins, losses and rank
     """   
 
-    def __init__(self, name: str) -> None:
-        """ initializes rank
-        >>> h = Profile(Hayden)
+    def __init__(self, name: str, i_d: int, board: Leaderboard) -> None:
+        """ 
+        >>> h = Profile(Hayden, 51, main_board)
         """
         self.__name = name
-        self.__rank = 0#placeholder for the rank fuction
+        self.__id = i_d
+        self.__rank = board.new_pro(self)
         self.__wins = 0
         self.__losses = 0
 
