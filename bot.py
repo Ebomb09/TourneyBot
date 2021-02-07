cfc74b556531a3815fad6975cecf2ea1136a293c
Ebomb09 <ebryson09@gmail.com>
2021-02-06 18:38:01 -0800

    Initial

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+
+
+def main():
+    client = MyClient()
+    client.run('token')
+
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+
+if __name__ == "__main__":
+    main()
